81ea0652042b1d042c4e2ba86f41e0156145473b
Israfil-Zekai-Yilmaz <153690204+Israfil-Zekai-Yilmaz@users.noreply.github.com>
2024-05-30 20:09:42 +0200

    Add files via upload
    I

---
+++ Script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('contactButton').addEventListener('click', function () {
+        window.location.href = 'ZekContact.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('project3').addEventListener('click', function () {
+        window.location.href = 'https://github.com/Israfil-Zekai-Yilmaz/Guess-Game';
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('project2').addEventListener('click', function () {
+        window.location.href = 'https://github.com/Israfil-Zekai-Yilmaz/10fastfingers';
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('project1').addEventListener('click', function () {
+        window.location.href = 'https://github.com/Israfil-Zekai-Yilmaz/Library-Store';
+    });
+});
+
+
